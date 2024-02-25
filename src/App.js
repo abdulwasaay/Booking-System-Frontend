6eadf67284f7f7e0fe2edc091c14a94a77ed6e08
Rabia Zubair <rabiazubair949@gmail.com>
2024-02-25 13:02:23 +0500

    Add navbar.jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import { createRoutesFromElements , createBrowserRouter, Route } from 'react-router';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    createBrowserRouter(
+        createRoutesFromElements(
+            <Route path='/'>
+
+            </Route>
+        )
+    )
 }
 
 export default App;
