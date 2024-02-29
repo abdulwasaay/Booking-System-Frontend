bf1d92de53ccde1887cb80421be134fc020a8a85
abdullah3-memon <abdullahmemon12564@gmail.com>
2024-02-29 13:11:16 +0500

    json data change with array of object

---
+++ src/components/flights/city2.js
@@ -0,0 +1,143 @@
+const citiesData2 = [
+    {
+      name: "Karachi",
+      shortForm: "KHI",
+      flightNumber: "PK101",
+      flightPrice: 1200,
+      otherParameters: "parameter1"
+    },
+    {
+      name: "Lahore",
+      shortForm: "LHE",
+      flightNumber: "PK102",
+      flightPrice: 1300,
+      otherParameters: "parameter2"
+    },
+    {
+      name: "Islamabad",
+      shortForm: "ISB",
+      flightNumber: "PK103",
+      flightPrice: 1100,
+      otherParameters: "parameter3"
+    },
+    {
+      name: "Rawalpindi",
+      shortForm: "RWP",
+      flightNumber: "PK104",
+      flightPrice: 1500,
+      otherParameters: "parameter4"
+    },
+    {
+      name: "Peshawar",
+      shortForm: "PEW",
+      flightNumber: "PK105",
+      flightPrice: 1250,
+      otherParameters: "parameter5"
+    },
+    {
+      name: "Quetta",
+      shortForm: "UET",
+      flightNumber: "PK106",
+      flightPrice: 1400,
+      otherParameters: "parameter6"
+    },
+    {
+      name: "Multan",
+      shortForm: "MUX",
+      flightNumber: "PK107",
+      flightPrice: 1150,
+      otherParameters: "parameter7"
+    },
+    {
+      name: "Faisalabad",
+      shortForm: "LYP",
+      flightNumber: "PK108",
+      flightPrice: 1350,
+      otherParameters: "parameter8"
+    },
+    {
+      name: "Hyderabad",
+      shortForm: "HDD",
+      flightNumber: "PK109",
+      flightPrice: 1200,
+      otherParameters: "parameter9"
+    },
+    {
+      name: "Sialkot",
+      shortForm: "SKT",
+      flightNumber: "PK110",
+      flightPrice: 1450,
+      otherParameters: "parameter10"
+    },
+    {
+      name: "Gujranwala",
+      shortForm: "GWD",
+      flightNumber: "PK111",
+      flightPrice: 1300,
+      otherParameters: "parameter11"
+    },
+    {
+      name: "Bahawalpur",
+      shortForm: "BHV",
+      flightNumber: "PK112",
+      flightPrice: 1350,
+      otherParameters: "parameter12"
+    },
+    {
+      name: "Jhang",
+      shortForm: "JHE",
+      flightNumber: "PK113",
+      flightPrice: 1400,
+      otherParameters: "parameter13"
+    },
+    {
+      name: "Sukkur",
+      shortForm: "SKZ",
+      flightNumber: "PK114",
+      flightPrice: 1400,
+      otherParameters: "parameter14"
+    },
+    {
+      name: "Dera Ghazi Khan",
+      shortForm: "DEA",
+      flightNumber: "PK115",
+      flightPrice: 1375,
+      otherParameters: "parameter15"
+    },
+    {
+      name: "Jacobabad",
+      shortForm: "JAG",
+      flightNumber: "PK116",
+      flightPrice: 1350,
+      otherParameters: "parameter16"
+    },
+    {
+      name: "Kohat",
+      shortForm: "OHT",
+      flightNumber: "PK117",
+      flightPrice: 1400,
+      otherParameters: "parameter17"
+    },
+    {
+      name: "Mianwali",
+      shortForm: "MWD",
+      flightNumber: "PK118",
+      flightPrice: 1375,
+      otherParameters: "parameter18"
+    },
+    {
+      name: "Murree",
+      shortForm: "MUR",
+      flightNumber: "PK119",
+      flightPrice: 1550,
+      otherParameters: "parameter19"
+    },
+    {
+      name: "Khanpur",
+      shortForm: "KRU",
+      flightNumber: "PK120",
+      flightPrice: 1450,
+      otherParameters: "parameter20"
+    }
+  ];
+  
