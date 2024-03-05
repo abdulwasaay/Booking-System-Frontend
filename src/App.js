40b02ea8a0145cbed843b6764787442118df4898
Abdul Wasay <wasaysaleem071@gmail.com>
2024-03-04 16:44:55 -0800

    login signup form added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Buses from './pages/Buses/buses';
 import BusOrderPage from './pages/Buses/BusesOrder';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path='/buses/' element={<Buses />} />
         <Route path='/buses/ordernow' element={<BusOrderPage />}/>
+        <Route path='/signup' element={<Signup />}/>
+        <Route path='/login' element={<Login />}/>
       </Routes>
     </BrowserRouter>
   )
